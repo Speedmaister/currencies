fb7cd4f1ee9418662c996a6277701ac97f28aeaa
stamo.petkov@gmail.com <stamo.petkov@gmail.com@db4b50b8-17ae-7008-12f6-ebfdd7cb10a1>
2013-09-01 11:13:30 +0000

    Data layer added

---
+++ trunk/Currency/js/data.js
@@ -0,0 +1,79 @@
+﻿/// <reference path="apiRequest.js" />
+/// <reference path="currencyAction.js" />
+(function () {
+    var roamingFolder = Windows.Storage.ApplicationData.current.roamingFolder;
+    var globalSettings;
+    var requester = new Currency.Utilities.ApiRequest();
+
+    roamingFolder.getFileAsync("settings.json").then(function (file) {
+        Windows.Storage.FileIO.readTextAsync(file).then(function (text) {
+            globalSettings = JSON.parse(text);
+        });
+    }, function () {
+        configureSettings();
+    });
+
+    function configureSettings() {
+        var clientInfo = new Currency.Utilities.ClientInfo(),
+                        toCurrencyCode = codeConvertor.createCountryToCurrency(),
+                        currencyCode;
+
+        globalSettings = defaultSettings.settings;
+
+        clientInfo.getData(function (data) {
+            var clientData = JSON.parse(data.responseText);
+            currencyCode = toCurrencyCode[clientData.countryCode];
+            if (currencyCode) {
+                globalSettings.baseCurrency = currencyCode;
+            }
+
+            roamingFolder.createFileAsync("settings.json",
+            Windows.Storage.CreationCollisionOption.replaceExisting).then(function (file) {
+                Windows.Storage.FileIO.writeTextAsync(file, JSON.stringify(globalSettings));
+            });
+        }, function (message) { });
+    }
+
+    function getLatestRates() {
+        var query = "base=" + globalSettings.baseCurrency;
+        var options = {
+            command: "getLatest",
+            query: query
+        }
+
+        return requester.getData(options);
+    }
+
+    function getHistoricalRates(date) {
+        var dateStr = padStr(date.getFullYear()) + "-" +
+                  padStr(1 + date.getMonth()) + "-" +
+                  padStr(date.getDate());
+
+        var query = "base=" + globalSettings.baseCurrency + "&date=" + dateStr;
+        var options = {
+            command: "getHistorical",
+            query: query
+        }
+
+        return requester.getData(options);
+    }
+
+    function padStr(i) {
+        return (i < 10) ? "0" + i : "" + i;
+    }
+
+    function getSettings() {
+
+    }
+
+    function setSettings() {
+
+    }
+
+    WinJS.Namespace.define("Currency.Data", {
+        getLatestRates: getLatestRates,
+        getHistoricalRates: getHistoricalRates,
+        getSettings: getSettings,
+        setSettings: setSettings
+    });
+}());
--- trunk/Currency/js/default.js
+++ trunk/Currency/js/default.js
@@ -10,19 +10,11 @@
     var activation = Windows.ApplicationModel.Activation;
     var nav = WinJS.Navigation;
     var globalSettings;
-    var roamingFolder = Windows.Storage.ApplicationData.current.roamingFolder;
-    var codeConvertor = new Currency.Utilities.CodeConverter();
 
     app.addEventListener("activated", function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-                roamingFolder.getFileAsync("settings.json").then(function (file) {
-                    Windows.Storage.FileIO.readTextAsync(file).then(function (text) {
-                        globalSettings = JSON.parse(text);
-                    });
-                }, function () {
-                    configureSettings();
-                });
+
             } else {
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
@@ -50,26 +42,7 @@
         app.sessionState.history = nav.history;
     };
 
-     function configureSettings() {
-        var clientInfo = new Currency.Utilities.ClientInfo(),
-                        toCurrencyCode = codeConvertor.createCountryToCurrency(),
-                        currencyCode;
-
-        globalSettings = defaultSettings.settings;
-
-        clientInfo.getData(function (data) {
-            var clientData = JSON.parse(data.responseText);
-            currencyCode = toCurrencyCode[clientData.countryCode];
-            if (currencyCode) {
-                globalSettings.baseCurrency = currencyCode;
-            }
-
-            roamingFolder.createFileAsync("settings.json",
-            Windows.Storage.CreationCollisionOption.replaceExisting).then(function (file) {
-                Windows.Storage.FileIO.writeTextAsync(file, JSON.stringify(globalSettings));
-            });
-        }, function (message) { });
-    }
+     
 
     app.start();
 })();
